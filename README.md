77ae3b127757d44fd20475f0013ab96a98f3bde7
Alamsyah Putra <muhammad.alamyah@gmail.com>
2021-01-31 23:45:27 +0700

    adding animation and stlye for custom dialog and logout with remove cache

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # testapplication
-Mockup E-Commerce android application with Kotlin and MVVM Design Pattern, sign in username & password, google and facebook integration.
+Mockup E-Commerce android application with Kotlin and MVVM Design Pattern.
+
+sign in username & password, google and facebook SDK integration.
+integration with API using okhttp and retrofit2, animation loading with lottie.
+
+1. login pages
+2. Home pages
+3. Search pages
+4. Detail Product pages
+5. History Purchase pages
+6. Dynamic Webview Fragment pages
+
+
--- app/src/main/java/com/sehatq/testapplication/feature/entrance/SplashActivity.kt
+++ app/src/main/java/com/sehatq/testapplication/feature/entrance/SplashActivity.kt
@@ -3,6 +3,7 @@ package com.sehatq.testapplication.feature.entrance
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import com.sehatq.testapplication.R
 import com.sehatq.testapplication.core.platform.BaseActivity
 import com.sehatq.testapplication.databinding.ActivitySplashBinding
 import com.sehatq.testapplication.feature.aut.view.LoginActivity
@@ -18,6 +19,10 @@ class SplashActivity : BaseActivity<ActivitySplashBinding>() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        bindView(R.layout.activity_splash)
+        binding.apply {
+            descTextView.text = getString(R.string.app_name)
+        }
         val handler = Handler()
         handler.postDelayed( Runnable {
             val intent = Intent(this,
